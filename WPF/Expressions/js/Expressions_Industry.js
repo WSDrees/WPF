dd996c8c79c348a894bf94d284feec2542b22204
WSDrees <SteveDrees@fullsail.edu>
2014-01-16 15:09:04 -0500

    Expression_Industry
    Removed months section

--- WPF/Expressions/js/Expressions_Industry.js
+++ WPF/Expressions/js/Expressions_Industry.js
@@ -6,12 +6,11 @@
 //Given how many months, weeks, days, hours, minutes, and seconds in a year.
 //Information from https://support.google.com/websearch/answer/3284611?hl=en#unitconverter
 
-var totalMonths = 12;
-var totalWeeks = 52.1775;
-var totalDays = 365.242;
-var totalHours = 8765.81;
-var totalMinutes = 525949;
-var totalSeconds = 31558152.96;
+var weeks = 52.1775;
+var days = 365.242;
+var hours = 8765.81;
+var minutes = 525949;
+var seconds = 31558152.96;
 
 //User Input
 var workHours = parseInt(prompt("How many hours a week do you spend at work?"));
@@ -20,27 +19,36 @@ var sleepTime = parseInt(prompt("How many hours a night do you sleep?"));
 var internetTime = parseInt(prompt("How many hours a day do you spend on the internet?"));
 var phoneTime = parseInt(prompt("How many hours a day do you spend on your phone?"));
 
-//Mathematical operations
+//Doing the Math
+//Total number of weeks
+
+//Total number of days
+
+//Total number of hours
+
+//Total number of minutes
+
+//Total number of seconds
 
 
 //Output
-//In a year your spend X months, X Weeks, X Days, X Hours, X Minutes, and X Seconds at work.
-console.log("In a year you spend" + + " months, " + + " weeks, " + + " days, " + + " hours "
-             + + " minutes, " + + " and " + + " seconds at work.");
+//In a year your spend X Weeks, X Days, X Hours, X Minutes, and X Seconds at work.
+console.log("In a year you spend" + + " weeks, " + + " days, " + + " hours "
+            + + " minutes, " + + " and " + + " seconds at work.");
 
 
 //In a year your spend X months, X Weeks, X Days, X Hours, X Minutes, and X Seconds traveling to work.
-console.log("In a year you spend" + + " months, " + + " weeks, " + + " days, " + + " hours "
-    + + " minutes, " + + " and " + + " traveling to work.");
+console.log("In a year you spend" + + " weeks, " + + " days, " + + " hours "
+             + + " minutes, " + + " and " + + " traveling to work.");
 
 //In a year your spend X months, X Weeks, X Days, X Hours, X Minutes, and X Seconds sleeping.
-console.log("In a year you spend" + + " months, " + + " weeks, " + + " days, " + + " hours "
-    + + " minutes, " + + " and " + + " sleeping.");
+console.log("In a year you spend" + + " weeks, " + + " days, " + + " hours "
+            + + " minutes, " + + " and " + + " sleeping.");
 
 //In a year your spend X months, X Weeks, X Days, X Hours, X Minutes, and X Seconds on the internet.
-console.log("In a year you spend" + + " months, " + + " weeks, " + + " days, " + + " hours "
-    + + " minutes, " + + " and " + + " on the internet.");
+console.log("In a year you spend"  + + " weeks, " + + " days, " + + " hours "
+            + + " minutes, " + + " and " + + " on the internet.");
 
 //In a year your spend X months, X Weeks, X Days, X Hours, X Minutes, and X Seconds on your phone.
-console.log("In a year you spend" + + " months, " + + " weeks, " + + " days, " + + " hours "
-    + + " minutes, " + + " and " + + " on your phone.");
+console.log("In a year you spend" + + " weeks, " + + " days, " + + " hours "
+            + + " minutes, " + + " and " + + " on your phone.");
