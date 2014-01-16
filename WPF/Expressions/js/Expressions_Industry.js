5e72506f4e6c19684c44960462da805a78acff2c
WSDrees <SteveDrees@fullsail.edu>
2014-01-16 15:24:18 -0500

    Expressions_Industry
    Adding comments to flowchart my idea

--- WPF/Expressions/js/Expressions_Industry.js
+++ WPF/Expressions/js/Expressions_Industry.js
@@ -2,34 +2,37 @@
  * Created by Steve Drees on 1/15/14.
  */
 
-//Time Management
-//Given how many months, weeks, days, hours, minutes, and seconds in a year.
+//Time Converter
+//Given how many weeks, days, hours, minutes, and seconds in a year.
 //Information from https://support.google.com/websearch/answer/3284611?hl=en#unitconverter
-
-var weeks = 52.1775;
-var days = 365.242;
-var hours = 8765.81;
-var minutes = 525949;
-var seconds = 31558152.96;
-
 //User Input
 var workHours = parseInt(prompt("How many hours a week do you spend at work?"));
 var workTravel = parseInt(prompt("How many hours a week do you spend traveling to and from work"));
-var sleepTime = parseInt(prompt("How many hours a night do you sleep?"));
-var internetTime = parseInt(prompt("How many hours a day do you spend on the internet?"));
-var phoneTime = parseInt(prompt("How many hours a day do you spend on your phone?"));
+var sleepTime = parseInt(prompt("How many hours a week do you sleep?"));
+var internetTime = parseInt(prompt("How many hours a week do you spend on the internet?"));
+var phoneTime = parseInt(prompt("How many hours a week do you spend on your phone?"));
+var week = 1;
+var day = 7;
+var hours = 168;
+var minutes = 10080;
+var seconds = 604800;
 
 //Doing the Math
-//Total number of weeks
+//7 days in a week, 24 hours in 1 day, 60 minutes in an hour, 60 seconds in a minute
+//Convert hours to weeks
+//Every 168 hours equals 1 week
 
-//Total number of days
 
-//Total number of hours
 
-//Total number of minutes
+//Convert hours to days
+//Every 24 hours equals 1 day
 
-//Total number of seconds
 
+//Convert hours to minutes
+//Every 1 hour equals 60 minutes
+
+//Convert hours to seconds
+// Every 1 hour equals 3600 seconds
 
 //Output
 //In a year your spend X Weeks, X Days, X Hours, X Minutes, and X Seconds at work.
