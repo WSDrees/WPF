43a3241f56ff845ef7a1204aa68c0d693e8b89df
WSDrees <SteveDrees@fullsail.edu>
2014-01-18 01:05:28 -0500

    New directory MISC
    Moved all class examples to one folder

---
+++ WPF/Misc/Intro to Variables/Class1_Arrays.js
@@ -0,0 +1,26 @@
+/**
+ * Created by wsdrees on 1/8/14.
+ */
+//var shoppingList= [];  Syntax for creating an ARRAY
+var shoppingList = ["Eggs","Chicken","brownRice","Vegetables"];
+console.log(shoppingList);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+console.log(shoppingList.length);  //Syntax to see how long the list is
+shoppingList[4] ="Formula";
+console.log(shoppingList.length);
+shoppingList.push("Bacon");
+console.log(shoppingList.length);
+console.log(shoppingList);
+shoppingList.pop();
+console.log(shoppingList);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+//console.log(shoppingList[5]);
+
---
+++ WPF/Misc/js/Functions.js
@@ -0,0 +1,18 @@
+/**
+ * Created by wsdrees on 1/17/14.
+ */
+
+//A function that takes in an array and returns the length of the array you pass in
+
+//function arrayLength()
+//{
+//    var world = ["Dog" , "Cat" , "Bird" , "Snake"];
+//    return world.length;
+//}
+//
+//console.log(arrayLength);
+
+function arrayLengthCounter(array1)
+{
+    return array1.length;
+}
