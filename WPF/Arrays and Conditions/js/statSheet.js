74b698c787050b3ee8b3d8e2ca04ef549d620c3f
WSDrees <SteveDrees@fullsail.edu>
2014-01-19 10:38:17 -0500

    statSheet
    Created a monster stat sheet

--- WPF/Arrays and Conditions/js/statSheet.js
+++ WPF/Arrays and Conditions/js/statSheet.js
@@ -2,10 +2,9 @@
  * Created by wsdrees on 1/17/14.
  */
 //Creating a stat sheet for a monster
-//User provides monster name
 
 //var type = 0;
-var monsterName = prompt("Name the monster");
+var monsterName = prompt("Name the monster");    //User provides monster name
 var str = 0;                                     //Strength
 var dex = 1;                                     //Dexterity
 var con = 2;                                     //Charisma
@@ -17,38 +16,39 @@ var min = 0;
 var max = 1;
 
 var stats=[[3,18],[3,18],[3,18],[3,18],[3,18],[3,18]];
+
 //Random stat for Strength
-console.log(Math.floor(
+console.log("Strength: \t\t" + (Math.floor(
     (stats[str][max] - stats[str][min])
         *Math.random())
-    +stats[str][min]);
+    +stats[str][min]));
 
 //Random stat for Dexterity
-console.log(Math.floor(
+console.log("Dexterity: \t\t" + (Math.floor(
     (stats[dex][max] - stats[dex][min])
         *Math.random())
-    +stats[dex][min]);
+    +stats[dex][min]));
 
 //Random stat for Charisma
-console.log(Math.floor(
+console.log("Charisma: \t\t" + (Math.floor(
     (stats[con][max] - stats[con][min])
         *Math.random())
-    +stats[con][min]);
+    +stats[con][min]));
 
 //Random stat for Constitution
-console.log(Math.floor(
+console.log("Constitution: \t" + (Math.floor(
     (stats[cha][max] - stats[cha][min])
         *Math.random())
-    +stats[cha][min]);
+    +stats[cha][min]));
 
 //Random stat for Wisdom
-console.log(Math.floor(
+console.log("Wisdom: \t\t" + (Math.floor(
     (stats[wis][max] - stats[wis][min])
         *Math.random())
-    +stats[wis][min]);
+    +stats[wis][min]));
 
 //Random stat for Intelligence
-console.log(Math.floor(
+console.log("Intelligence: \t" + (Math.floor(
     (stats[int][max] - stats[int][min])
         *Math.random())
-    +stats[int][min]);
+    +stats[int][min]));
