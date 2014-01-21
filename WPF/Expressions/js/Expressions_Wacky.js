746f04f8d4f90e80ac8e12e88acfd29c4298d690
WSDrees <SteveDrees@fullsail.edu>
2014-01-21 18:03:50 -0500

    Expressions Assignment
    Final

--- WPF/Expressions/js/Expressions_Wacky.js
+++ WPF/Expressions/js/Expressions_Wacky.js
@@ -7,8 +7,12 @@ var firstNumber = parseFloat(prompt("Enter a number: "));                       
 var secondNumber = parseFloat(prompt("Enter a second number: "));                   // User inputs second number
 var dividedBy = parseInt(firstNumber / secondNumber);                               // First number divided by the second
 var remainder = parseInt(firstNumber % secondNumber);                               // Provides the remainder of the two numbers provided by the user
+var sentenceA = "Your first number ";                                               // Sentence structureA
+var sentenceB = " divided by ";                                                     // Sentence structureB
+var sentenceC = " equals ";                                                         // Sentence structureC
+var sentenceD = " and has a remainder of ";                                         // Sentence structureD
+var sentenceE = ".";                                                                // Sentence structureE
 
 //Output
 //Your first number X divided by X is X and has a remainder of X.
-console.log("Your first number " + firstNumber + " divided by " +secondNumber + " is " + dividedBy +
-            " and has a remainder of " + remainder + ".");
+console.log(sentenceA + firstNumber + sentenceB +secondNumber + sentenceC + dividedBy + sentenceD + remainder + sentenceE);
