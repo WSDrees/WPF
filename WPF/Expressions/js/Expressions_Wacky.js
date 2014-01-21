559714857f1cbd162d55f0d69d54f1e21d5e8034
WSDrees <SteveDrees@fullsail.edu>
2014-01-21 16:49:22 -0500

    Expressions Assignment
    Expression_Wacky

--- WPF/Expressions/js/Expressions_Wacky.js
+++ WPF/Expressions/js/Expressions_Wacky.js
@@ -2,4 +2,12 @@
  * Created by Steve Drees on 1/15/14 Assignment: Expressions_Wacky.
  */
 
+//Find the remainder
+var firstNumber = parseFloat(prompt("Enter a number: "));
+var secondNumber = parseFloat(prompt("Enter a second number: "));
+var dividedBy = parseInt(firstNumber / secondNumber);
+var remainder = firstNumber % secondNumber;
+
+console.log("Your first number " + firstNumber + " divided by " +secondNumber + " is " + dividedBy +
+            " and has a remainder of " + parseFloat(remainder));
 
