cf6dd9f53b269cee7bc340cb613b040f8197af02
WSDrees <SteveDrees@fullsail.edu>
2014-01-20 21:14:27 -0500

    Conditionals
    Update

--- WPF/Conditionals/js/Conditionals_Wacky.js
+++ WPF/Conditionals/js/Conditionals_Wacky.js
@@ -1,3 +1,9 @@
 /**
- * Created by wsdrees on 1/15/14.
+ * Created by Steve Drees on 1/15/14, Conditionals_Wacky.
  */
+
+
+//Use Modulus
+//Count odd or even
+var userInput = prompt("Would you like to count in even or odd numbers?");
+var whatNumber = prompt("What number would you like to count to?");
