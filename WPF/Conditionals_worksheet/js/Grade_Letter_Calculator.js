1b7777cc0ab1d108e40bcab8d78f8a1c8de45e3f
WSDrees <SteveDrees@fullsail.edu>
2014-01-22 03:34:43 -0500

    Conditionals_worksheet
    Updating code and comments

--- WPF/Conditionals_worksheet/js/Grade_Letter_Calculator.js
+++ WPF/Conditionals_worksheet/js/Grade_Letter_Calculator.js
@@ -1,15 +1,15 @@
 /**
  * Created by Steve Drees on 1/15/14 Activity: Grade_Letter_Calculator.
  */
-//Grading scale:   GPA
-//A+ = 95 - 100% = 4.0
-//A  = 90 - 94%  = 3.5
-//B+ = 84 - 89%  = 3.0
-//B  = 80 - 84%  = 2.5
-//C+ = 76 - 79%  = 2.0
-//C  = 73 - 75%  = 1.5
-//D  = 70 - 72%  = 1.0
-//F  = 0 - 69%   = 0.0
+//FullSail Grading scale:
+//A+ = 95 - 100%
+//A  = 90 - 94%
+//B+ = 84 - 89%
+//B  = 80 - 84%
+//C+ = 76 - 79%
+//C  = 73 - 75%
+//D  = 70 - 72%
+//F  = 0  - 69%
 //User input
 
 var studentNumberGrade = parseInt(prompt("What was your average for your class? (0 - 100)"));        //User inputs grade
@@ -26,18 +26,18 @@ var g = "D";
 if(studentNumberGrade > 100 || studentNumberGrade < 0){
     console.log("Invalid grade!!!!");
 }else if(studentNumberGrade >= 95){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +a+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +a+ " in the class.");
 }else if(studentNumberGrade >= 90){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +b+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +b+ " in the class.");
 }else if(studentNumberGrade >= 84){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +c+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +c+ " in the class.");
 }else if(studentNumberGrade >= 80){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +d+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +d+ " in the class.");
 }else if(studentNumberGrade >= 76){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +e+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +e+ " in the class.");
 }else if(studentNumberGrade >= 73){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +f+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +f+ " in the class.");
 }else if(studentNumberGrade >= 70){
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +g+ " in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) " +g+ " in the class.");
 }else
-    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) F in the class");
+    console.log("You have " +studentNumberGrade+ "%, which mean you have earned a(n) F in the class.");
