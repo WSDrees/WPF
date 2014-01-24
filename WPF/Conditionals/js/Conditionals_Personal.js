eddd4e4f6aa4bbbffbdd5c7f7c1b6c05da1d818f
WSDrees <SteveDrees@fullsail.edu>
2014-01-24 16:57:52 -0500

    Conditionals Assignment
    Updates

--- WPF/Conditionals/js/Conditionals_Personal.js
+++ WPF/Conditionals/js/Conditionals_Personal.js
@@ -1,3 +1,4 @@
 /**
  * Created by Steve Drees on 1/15/14, Assignment: Conditionals_Personal.
  */
+//Do a math problem of user choice
--- WPF/Conditionals/js/Conditionals_Wacky.js
+++ WPF/Conditionals/js/Conditionals_Wacky.js
@@ -2,3 +2,44 @@
  * Created by Steve Drees on 1/15/14, Assignment: Conditionals_Wacky.
  */
 
+//Creating a sandwich
+var bread = prompt("What type of bread would you like? (White or Wheat?").toLowerCase();
+var meat = prompt("What type of meat would you like? (Ham or Turkey?").toLowerCase();
+var topping1 = prompt("Would you like lettuce? (Yes or No)").toLowerCase();
+var topping2 = prompt("Would you like tomato? (Yes or No)").toLowerCase();
+var topping3 = prompt("Would you like onion? (Yes or No").toLowerCase();
+var topping4 = prompt("Would you like peppers? (Yes or No)").toLowerCase();
+var lettuce = "lettuce";
+var tomato = "tomato";
+var onion = "onion";
+var peppers = "peppers";
+var dressing1 = prompt("Would you like ranch dressing? (Yes or No)").toLowerCase();
+var dressing2 = prompt("Would you like oil and vinegar? (Yes or No)").toLowerCase();
+var ranch = "ranch";
+var oilAndVinegar = "oil and vinegar";
+
+//Bread type
+if(bread !== "white" || bread !== "wheat"){
+
+}else{
+
+}
+
+//Meat type
+if(meat !== "ham" || meat !== "turkey"){
+
+}else{
+
+}
+
+//Toppings
+if (topping1 || topping2 || topping3 || topping4 !== "yes" || "no"){
+
+}else if(topping1 == "yes"){
+
+}
+
+//Dressing
+
+//Output
+//You ordered a X sandwich on X bread with X, X, X, X, and X. Enjoy!
