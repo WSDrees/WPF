1f57e826bdb543738f5268575ee8951d8c5340f0
WSDrees <SteveDrees@fullsail.edu>
2014-01-25 02:20:51 -0500

    Conditionals Assignment
    Trying to figure out why “oil and vinegar” always show up, when the
    answer is no

--- WPF/Conditionals/js/Conditionals_Wacky.js
+++ WPF/Conditionals/js/Conditionals_Wacky.js
@@ -13,74 +13,69 @@ var dressing1 = prompt("Would you like ranch dressing? (Yes or No)").toLowerCase
 var dressing2 = prompt("Would you like oil and vinegar? (Yes or No)").toLowerCase();
 var breadAnswer = bread;
 var meatAnswer = meat;
-var topping1Answer = "";
-var topping2Answer = "";
-var topping3Answer = "";
-var topping4Answer = "";
-var dressing1Answer = "";
-var dressing2Answer = "";
-var lettuce = "";
-var tomato = "";
-var onion = "";
-var peppers = "";
-var ranch = "";
-var oilAndVinegar = "";
+var topping1Answer;
+var topping2Answer;
+var topping3Answer;
+var topping4Answer;
+var dressing1Answer;
+var dressing2Answer;
+
 
 //Bread type
-if(bread == "white" || bread == "wheat"){
-    breadAnswer = bread;
+if(bread == "white" || bread == "wheat"){                                               //User's two choices
+    breadAnswer = bread;                                                                //User choices between white and wheat bread
 }else{
-    console.log("We can not make your sandwich without bread!");                        //Error message
+    breadAnswer = "";                                                                   //User does NOT want any bread. WEIRD, but okay
 }
 
 //Meat type
-if(meat == "ham" || meat == "turkey"){
-    meatAnswer = meat;
+if(meat == "ham" || meat == "turkey"){                                                  //User's choice
+    meatAnswer = meat;                                                                  //User choices between ham and turkey for their meat type
 }else{
-    console.log("We can not make your sandwich without meat");                          //Error message
+   meatAnswer = "";                                                                     //User does NOT get any meat
 }
 
 //Lettuce topping
-if ((topping1 == "yes") || (topping1 == "y")){
-    topping1Answer = " with lettuce, ";
+if ((topping1 == "yes") || (topping1 == "y")){                                          //Does the user want lettuce? (yes OR y)
+    topping1Answer = " with lettuce";                                                   //User adds lettuce
 }else{
-    lettuce = "";                                                                       //If user doesn't want lettuce, then he doesn't get lettuce
+    topping1Answer = "";                                                                //User does NOT get lettuce
 }
 
 //Tomato topping
-if ((topping2 == "yes") || (topping2 == "y")){
-    topping2Answer = ", tomato";
+if ((topping2 == "yes") || (topping2 == "y")){                                          //Does the user want tomatoes? (yes OR y)
+    topping2Answer = ", tomatoes";                                                      //User adds tomatoes
 }else{
-    tomato = "";                                                                       //If user doesn't want lettuce, then he doesn't get lettuce
+    topping2Answer = "";                                                                //User does NOT get tomatoes
 }
 
 //Onion topping
-if ((topping3 == "yes") || (topping3 == "y")){
-    topping3Answer = ", onion";
+if ((topping3 == "yes") || (topping3 == "y")){                                          //Does the user want onions? (yes OR y)
+    topping3Answer = ", onions";                                                        //User adds onions
 }else{
-    onion = "";                                                                       //If user doesn't want lettuce, then he doesn't get lettuce
+    topping3Answer = "";                                                                //User does NOT get onions
 }
 
 //Peppers topping
-if ((topping4 == "yes") || (topping4 == "y")){
-    topping4Answer = ", peppers";
+if ((topping4 == "yes") || (topping4 == "y")){                                          //Does the user want peppers? (yes OR y)
+    topping4Answer = ", peppers";                                                       //User adds peppers
 }else{
-    peppers = "";                                                                       //If user doesn't want lettuce, then he doesn't get lettuce
+    topping4Answer = "";                                                                //User does NOT get peppers.
 }
 
-if ((dressing1 == "yes" || dressing1 =="y")){
-    dressing1Answer = ", ranch";
+if ((dressing1 == "yes" || dressing1 =="y")){                                           //Does the user want ranch dressing? (yes OR y)
+    dressing1Answer = ", ranch";                                                        //User adds ranch dressing
 }else{
-    ranch = "";
+    dressing1Answer = "";                                                               //User does NOT get ranch dressing
 }
 
-if ((dressing2 == "yes" || dressing2 + "y")){
-    dressing2Answer = ", oil and vinegar. ";
+if ((dressing2 == "yes" || dressing2 + "y")){                                           //Does the user want oil and vinegar? (yes OR y)
+    dressing2Answer = ", oil and vinegar. ";                                            //User adds oil and vinegar
 }else {
-    oilAndVinegar = "";
+    dressing2Answer = "";                                                               //User does NOT get oil and vinegar
 }
 
 //Dressing
 //Output
 //You ordered a X sandwich on X bread with X, X, X, X, and X. Enjoy!
-console.log("You ordered a(n) " +meat+ " sandwich on " +bread+topping1Answer+topping2Answer+topping3Answer+topping4Answer+dressing1Answer+dressing2Answer+ "Enjoy!");
+console.log("You ordered a " +meatAnswer+ " sandwich on " + breadAnswer+ " with " +topping1Answer+topping2Answer+topping3Answer+topping4Answer+dressing1Answer+dressing2Answer+ " Enjoy!");
