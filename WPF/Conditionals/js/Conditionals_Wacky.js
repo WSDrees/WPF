c0b30086e26441c2ef4646de3e39570327580b8f
WSDrees <SteveDrees@fullsail.edu>
2014-01-25 03:57:24 -0500

    Conditionals Assignment
    Trying to figure out why the last if statement isn’t working: DEBUG

--- WPF/Conditionals/js/Conditionals_Wacky.js
+++ WPF/Conditionals/js/Conditionals_Wacky.js
@@ -3,22 +3,22 @@
  */
 
 //Creating a sandwich
-var bread = prompt("What type of bread would you like? (White or Wheat?").toLowerCase();
-var meat = prompt("What type of meat would you like? (Ham or Turkey?").toLowerCase();
-var topping1 = prompt("Would you like lettuce? (Yes or No)").toLowerCase();
-var topping2 = prompt("Would you like tomato? (Yes or No)").toLowerCase();
-var topping3 = prompt("Would you like onion? (Yes or No)").toLowerCase();
-var topping4 = prompt("Would you like peppers? (Yes or No)").toLowerCase();
-var dressing1 = prompt("Would you like ranch dressing? (Yes or No)").toLowerCase();
-var dressing2 = prompt("Would you like oil and vinegar? (Yes or No)").toLowerCase();
-var breadAnswer = bread;
-var meatAnswer = meat;
-var topping1Answer;
-var topping2Answer;
-var topping3Answer;
-var topping4Answer;
-var dressing1Answer;
-var dressing2Answer;
+var bread = prompt("What type of bread would you like? (White or Wheat?").toLowerCase();            //User prompt: White or Wheat?
+var meat = prompt("What type of meat would you like? (Ham or Turkey?").toLowerCase();               //User prompt: Ham or Turkey?
+var topping1 = prompt("Would you like lettuce? (Yes or No)").toLowerCase();                         //User prompt: Add lettuce? (Yes or No)
+var topping2 = prompt("Would you like tomato? (Yes or No)").toLowerCase();                          //User prompt: Add tomato? (Yes or No)
+var topping3 = prompt("Would you like onion? (Yes or No)").toLowerCase();                           //User prompt: Add onion? (Yes or No)
+var topping4 = prompt("Would you like peppers? (Yes or No)").toLowerCase();                         //User prompt: Add peppers? (Yes or No)
+var dressing1 = prompt("Would you like ranch dressing? (Yes or No)").toLowerCase();                 //User prompt: ranch dressing? (Yes or No)
+var dressing2 = prompt("Would you like oil and vinegar? (Yes or No)").toLowerCase();                //User prompt: oil and vinegar? (Yes or No)
+var breadAnswer = bread;                                                                            //Store user's bread selection (White or Wheat)
+var meatAnswer = meat;                                                                              //Store user's meat selection (Ham or turkey)
+var topping1Answer;                                                                                 //User's choice on lettuce
+var topping2Answer;                                                                                 //User's choice on tomato
+var topping3Answer;                                                                                 //User's choice on onion
+var topping4Answer;                                                                                 //User's choice on peppers
+var dressing1Answer;                                                                                //User's choice on ranch dressing
+var dressing2Answer;                                                                                //User's choice on oil and vinegar dressing
 
 
 //Bread type
@@ -37,7 +37,7 @@ if(meat == "ham" || meat == "turkey"){                                          
 
 //Lettuce topping
 if ((topping1 == "yes") || (topping1 == "y")){                                          //Does the user want lettuce? (yes OR y)
-    topping1Answer = " with lettuce";                                                   //User adds lettuce
+    topping1Answer = " lettuce";                                                        //User adds lettuce
 }else{
     topping1Answer = "";                                                                //User does NOT get lettuce
 }
@@ -63,19 +63,21 @@ if ((topping4 == "yes") || (topping4 == "y")){                                  
     topping4Answer = "";                                                                //User does NOT get peppers.
 }
 
-if ((dressing1 == "yes" || dressing1 =="y")){                                           //Does the user want ranch dressing? (yes OR y)
+//Ranch dressing?
+if ((dressing1 == "yes") || (dressing1 =="y")){                                         //Does the user want ranch dressing? (yes OR y)
     dressing1Answer = ", ranch";                                                        //User adds ranch dressing
 }else{
     dressing1Answer = "";                                                               //User does NOT get ranch dressing
 }
 
-if ((dressing2 == "yes" || dressing2 + "y")){                                           //Does the user want oil and vinegar? (yes OR y)
+//Oil and vinegar?
+if ((dressing2 == "yes") || (dressing2 + "y")){                                         //Does the user want oil and vinegar? (yes OR y)
     dressing2Answer = ", oil and vinegar. ";                                            //User adds oil and vinegar
 }else {
-    dressing2Answer = "";                                                               //User does NOT get oil and vinegar
+     dressing2Answer = "";                                                              //User does NOT get oil and vinegar
 }
 
-//Dressing
+
 //Output
 //You ordered a X sandwich on X bread with X, X, X, X, and X. Enjoy!
-console.log("You ordered a " +meatAnswer+ " sandwich on " + breadAnswer+ " with " +topping1Answer+topping2Answer+topping3Answer+topping4Answer+dressing1Answer+dressing2Answer+ " Enjoy!");
+console.log("You ordered a " +meatAnswer+ " sandwich on " + breadAnswer+topping1Answer+topping2Answer+topping3Answer+topping4Answer+dressing1Answer+dressing2Answer+ " Enjoy!");
