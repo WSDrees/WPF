5452cfa69be6cbd94dba6a43c7ad7a84c6245953
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 13:07:27 -0500

    Conditionals Assignment
    Conditionals_Personal

--- WPF/Conditionals/js/Conditionals_Personal.js
+++ WPF/Conditionals/js/Conditionals_Personal.js
@@ -1,28 +1,13 @@
 /**
  * Created by Steve Drees on 1/15/14, Assignment: Conditionals_Personal.
  */
-//Do a math problem of user choice
+//Old enough to gamble
 
-//Calculations
-var mathOperator1 = parseFloat(prompt("What mathematical operation would you like to perform? (*, /, +. -)"));                      //User input's what math they want to do
-var input1 = parseFloat(prompt("Input your first number:"));
-var input2 = parseFloat(prompt("Input your second number:"));
+var userAge = parseInt(prompt("How old are you?"));
 
-//if Multiplication
-if(mathOperator1 == "*"){
-    console.log("You entered " + input1+ " times " +input2+ " which equals " +(input1 * input2));
-}
-//else if Division (/)
-else if((mathOperator1 == "/") && (input1 >= input2)){
-    console.log("You entered " + input1+ " divided by " +input2+ " which equals " +(input1 / input2) + " with a remainder of " +(input1%input2));
-}else if((mathOperator1 == "/") && (input1 <= input2)){
-    console.log("You entered " + input1+ " divided by " +input2+ " which equals " +(input2 / input1) + " with a remainder of " +(input2%input1));
-}
-//else if Addition (+)
-else if(mathOperator1 == "+"){
-    console.log("You entered " + input1+ " plus " +input2+ " which equals " +(input1 + input2));
-}
-//else if Subtraction (-)
-else if(mathOperator1 == "-"){
-    console.log("You entered " + input1+ " minus " +input2+ " which equals " +(input1 - input2));
-}
+if(userAge >= 21){
+    console.log("Since you are " +userAge+ " you are able to gamble and drink.");
+} else if (userAge >= 18){
+    console.log("Since you are " +userAge+ " you are able to gamble.");
+} else
+    console.log("Sorry kid you are too young to enter.");
