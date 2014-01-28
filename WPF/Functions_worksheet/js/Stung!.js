d1cc7747fd7767225f133a49e6299fb62f2ab07d
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 21:54:52 -0500

    Functions_worksheet
    Setup of function

--- WPF/Functions_worksheet/js/Stung!.js
+++ WPF/Functions_worksheet/js/Stung!.js
@@ -1,3 +1,16 @@
 /**
- * Created by Steve Drees on 1/27/14.
+ * Created by Steve Drees on 1/27/14, Function_worksheet: Stings.
  */
+
+//Given it takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+
+function calcStings(){
+    var victimType = prompt("What kind of animal is it?");
+    var victimWeight = parseFloat(prompt("How much does the animal weight? (In lbs)"));
+    var stingsPerPound = 8.666666667;
+    var toDeath = (victimWeight*stingsPerPound).toFixed(2);
+    console.log("It takes " +toDeath+ " bee stings to kill a " +victimWeight+ " pound " +victimType+ '.');
+
+}
+
+calcStings();
