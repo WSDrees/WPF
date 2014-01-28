9126e3d7fd77a4a698b1b52c0bbec537618febee
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 21:41:52 -0500

    Functions_worksheet
    Writing the code to calculate the circumference.

--- WPF/Functions_worksheet/js/Circumference.js
+++ WPF/Functions_worksheet/js/Circumference.js
@@ -1,3 +1,13 @@
 /**
- * Created by wsdrees on 1/27/14.
+ * Created by Steve Drees on 1/27/14 Functions_worksheet: Circumference.
  */
+
+//Function name and parameters
+function calcCircumference(){
+    var pie = 3.14159;
+    var radius = parseFloat(prompt("What is the radius of the circle?")).toFixed(2);
+    var circumference = (2*pie*radius).toFixed(2);
+    console.log(circumference);
+}
+
+calcCircumference();
