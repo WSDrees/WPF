a5b7b8b82734f106e6f8a7137cd9a12549e6f78d
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 22:47:51 -0500

    Functions_Assignment
    Creating ideas

--- WPF/Functions/js/Functions_Industry.js
+++ WPF/Functions/js/Functions_Industry.js
@@ -2,4 +2,21 @@
  * Created by Steve Drees on 1/27/14, Assignment: Functions_Industry.
  */
 
+//How much time do you spend at work, on the internet, traveling, and on the phone.
 
+var lDays = 366;
+var days = 356;
+var hours = 8765.81;
+var lHours = 8784;
+var minutes = 525949;
+var lMinutes = 527040;
+var seconds = 31536000;
+var lSeconds = 31622400;
+
+function year(){
+
+}
+
+function leapYear(){
+
+}
--- WPF/Functions/js/Functions_Personal.js
+++ WPF/Functions/js/Functions_Personal.js
@@ -3,3 +3,12 @@
  */
 
 
+//How long have you been alive?
+//Add leap year
+
+var month = parseInt(prompt("What month were you born? (Ex: Jan = 1)"));
+var day = parseInt(prompt("What day were you born? (1 - 31)"));
+var year = parseInt(prompt("What year were you born? (Ex: 1980)"));
+
+var days = 356;
+var lDays = 366;
