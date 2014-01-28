b39fd6fb2935f8f4b4067bdd27e131c9658ac6f7
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 22:50:25 -0500

    Functions_Assignment
    Functions_Wacky - How much change to you have?

--- WPF/Functions/js/Functions_Wacky.js
+++ WPF/Functions/js/Functions_Wacky.js
@@ -2,4 +2,10 @@
  * Created by Steve Drees on 1/27/14, Assignment: Functions_Wacky.
  */
 
+// How much change do you have?
+//Quarters + Dimes + Nickles + Pennies
+//Every 100 cents equals have a dollar
 
+function change(){
+    
+}
