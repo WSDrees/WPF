5683a07cd5b909df8193a69773fa29dd418c5190
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 23:08:38 -0500

    Functions_Wacky
    Updates

--- WPF/Functions/js/Functions_Wacky.js
+++ WPF/Functions/js/Functions_Wacky.js
@@ -7,5 +7,16 @@
 //Every 100 cents equals have a dollar
 
 function change(){
-    
+    var quarterInput = parseInt(prompt("How many quarters do you have?"));
+    var dimeInput = parseInt(prompt("How many dimes do you have?"));
+    var nickleInput = parseInt(prompt("How many nickels do you have?"));
+    var pennyInput = parseInt(prompt("How many pennies do you have?"));
+    var quarter = quarterInput*0.25;
+    var dime = dimeInput*0.10;
+    var nickle = nickleInput*0.05;
+    var penny = pennyInput*0.01;
+    var dollar = quarter+dime+nickle+penny;
+    console.log("You have a total of " +dollar+ " dollars. Don't spend it all in one place");
 }
+
+//OUTPUT: You have a total of X dollars. Don't spend it all in one place
