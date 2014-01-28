0fadf5622245b2e9fcc2a31dfb1beed8a2c82adb
WSDrees <SteveDrees@fullsail.edu>
2014-01-27 21:19:00 -0500

    Conditionals Assignment
    Finishing up code

--- WPF/Conditionals/js/Conditionals_Personal.js
+++ WPF/Conditionals/js/Conditionals_Personal.js
@@ -4,11 +4,12 @@
 
 //Are you old enough to gamble
 
-var userAge = parseInt(prompt("How old are you?"));                                                                     //User input's age
+var userAge = parseInt(prompt("How old are you? (Example: 20)"));                                                        //User input's age
 
-if(userAge >= 21){
-    console.log("Since you are " +userAge+ " you are able to gamble and drink.");                                       //Evaluate to ensure user is old enough to gamble && drink
-} else if (userAge >= 18){
-    console.log("Since you are " +userAge+ " you are able to gamble.");                                                 //Evaluate to make sure user is old enough to gamble
-} else
-    console.log("Sorry kid you are too young to enter.");                                                               //Evaluate to inform user they are too young to enter
+if(userAge >= 21){                                                                                                      //Evaluate to ensure user is old enough to gamble && drink
+    console.log("Since you are " +userAge+ " you are able to gamble and drink.");
+}
+//Evaluate to make sure user is old enough to gamble
+//If NOT inform user they are too young to enter
+(userAge >=18) ? console.log("Since you are " +userAge+ " you are able to gamble.") : console.log("Sorry kid you are too young to enter.");
+
