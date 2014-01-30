b6cbba2c97deb8f4d26df05203f9e0335d469bd8
WSDrees <SteveDrees@fullsail.edu>
2014-01-30 17:35:29 -0500

    Functions Assignment
    Finished assignment

--- WPF/Functions/js/Functions_Industry.js
+++ WPF/Functions/js/Functions_Industry.js
@@ -2,47 +2,14 @@
  * Created by Steve Drees on 1/27/14, Assignment: Functions_Industry.
  */
 
-//Car model, gas type and mpg
+function totalMiles(){
+    var mpg1 = parseFloat(prompt("How many miles did you get on your first tank of gas?"));
+    var mpg2 = parseFloat(prompt("How many miles did you get on your second tank of gas?"));
+    var mpg3 = parseFloat(prompt("How many miles did you get on your third tank of gas?"));
+    var mpg4 = parseFloat(prompt("How many miles did you get on your fourth tank of gas?"));
+    var mpg5 = parseFloat(prompt("How many miles did you get on your fifth tank of gas?"));
+    console.log("You average " +(mpg1+mpg2+mpg3+mpg4+mpg5)/5+ " mpg.");
 
-//Car A, B, C
-var carType = prompt("Choose a car: \nS = Small\nM = Mid-size\nT = Truck\nS = SUV").toUpperCase();
-var gasType = prompt("Choose a type of gas: \nR = Regular\nM = Mid\nP = Premium\nD = Diesel").toUpperCase();
-
-//Average size found via Google
-var smallCarGasTank = 11;                                   //Average gas tank size of a small car
-var midSizeCarGasTank = 15;                                 //Average gas tank size of a mid size car
-var truckGasTank = 22.5;                                    //Average gas tank size of a truck
-var suvGasTank = 22.5;                                      //Average gas tank size of a SUV
-
-//Information from fuelgagereport.com on 1/30/14
-//Current Avg. 	Reg: $3.275 	Mid: $3.456 	Premium $3.624 	    Diesel: $3.890
-//Gas type Reg, Mid, Pre, and Diesel
-var gasReg = 3.275;
-var gasMid = 3.456;
-var gasPrem = 3.624;
-var diesel = 3.89;
-
-
-function gasTankSize(smallCarGasTank, midSizeCarGasTank, truckGasTank, suvGasTank){
-    if(carType == "S"){
-        return smallCarGasTank;
-    }else if(carType == "M"){
-        return midSizeCarGasTank;
-    }else if(carType == "T"){
-        return truckGasTank;
-    }else if(carType == "S"){
-        return suvGasTank;
-    }
 }
 
-function gasRate(gasReg, gasMid, gasPrem, diesel){
-    if(gasType == "R"){
-        return gasReg;
-    }else if(gasType =="M"){
-        return gasMid;
-    }else if(gasType =="P"){
-        return gasPrem;
-    }else if(gasType == "D"){
-        return diesel;
-    }
-}
+totalMiles();
