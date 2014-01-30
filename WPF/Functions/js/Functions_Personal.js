1627574641e86407c8dce30d7f8f7b9eb2764eb7
WSDrees <SteveDrees@fullsail.edu>
2014-01-29 23:27:02 -0500

    Function Assignment
    New ideas:
    
    Functions_Industry - Miles per gallon on which model vehicle.
    Function_Personal - Calories burned during specific workouts.

--- WPF/Functions/js/Functions_Personal.js
+++ WPF/Functions/js/Functions_Personal.js
@@ -2,13 +2,9 @@
  * Created by Steve Drees on 1/27/14, Assignment: Functions_Personal.
  */
 
+//How many calories burned
+//Did you walk, run, bike, swim?
+//Pace, and time
+//Weight
 
-//How long have you been alive?
-//Add leap year
 
-var month = parseInt(prompt("What month were you born? (Ex: Jan = 1)"));
-var day = parseInt(prompt("What day were you born? (1 - 31)"));
-var year = parseInt(prompt("What year were you born? (Ex: 1980)"));
-
-var days = 356;
-var lDays = 366;
