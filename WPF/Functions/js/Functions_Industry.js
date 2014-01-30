06366fc82b879d707e7f0298250c46cc86cd0b80
WSDrees <SteveDrees@fullsail.edu>
2014-01-30 16:14:17 -0500

    Functions Assignment
    Setting up variables

--- WPF/Functions/js/Functions_Industry.js
+++ WPF/Functions/js/Functions_Industry.js
@@ -5,16 +5,18 @@
 //Car model, gas type and mpg
 
 //Car A, B, C
-var makeOfCar = prompt("What is the make of car(s)?");
-var gasType = prompt("What type of gas does your car use? \nR = Regular\nM = Mid\nP = Premium\nD = Diesel");
-
-
+var makeOfCar = prompt("Choose a type of car: \nF = Ford\n H = Honda\n T = Toyota");
+var carSize = prompt("Choose a car: \nS = Small\nM = Mid-size\nT = Truck\nS = SUV");
+var gasType = prompt("Choose a type of gas: \nR = Regular\nM = Mid\nP = Premium\nD = Diesel");
+var whileCondition = true;
+var smallCar = 11;                                  //Average gas tank size of a small car
+var midSizeCar = 15;                                //Average gas tank size of a mid size car
+var truck = 22.5;                                   //Average gas tank size of a truck
+var suv = 22.5;                                     //Average gas tank size of a SUV
 
 //Information from fuelgagereport.com on 1/30/14
 //Current Avg. 	Reg: $3.275 	Mid: $3.456 	Premium $3.624 	    Diesel: $3.890
 //Gas type Reg, Mid, Pre, and Diesel
-
-
 var gasReg = 3.275;
 var gasMid = 3.456;
 var gasPrem = 3.624;
@@ -22,5 +24,9 @@ var diesel = 3.89;
 
 
 //Miles per gallon
+var mpgSmallCar = "";
+var mpgMidCar = "";
+var mpgTruck = "";
+var mpgSUV = "";
 //Gas tank size
 
