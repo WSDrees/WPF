7cf9ba6588abf87d80c85b08cce39c129d43d0de
WSDrees <SteveDrees@fullsail.edu>
2014-01-30 19:30:03 -0500

    Functions Assignment
    Comments

--- WPF/Functions/js/Functions_Industry.js
+++ WPF/Functions/js/Functions_Industry.js
@@ -3,11 +3,18 @@
  */
 
 function totalMiles(){
+    //User input miles
     var mpg1 = parseFloat(prompt("How many miles did you get on your first tank of gas?"));
+    //User input miles
     var mpg2 = parseFloat(prompt("How many miles did you get on your second tank of gas?"));
+    //User input miles
     var mpg3 = parseFloat(prompt("How many miles did you get on your third tank of gas?"));
+    //User input miles
     var mpg4 = parseFloat(prompt("How many miles did you get on your fourth tank of gas?"));
+    //User input miles
     var mpg5 = parseFloat(prompt("How many miles did you get on your fifth tank of gas?"));
+
+
     console.log("You average " +(mpg1+mpg2+mpg3+mpg4+mpg5)/5+ " mpg.");
 }
 
