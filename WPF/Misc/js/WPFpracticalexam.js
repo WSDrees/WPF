17795f4049456b68acf1eeca7734d5b953525dd1
WSDrees <SteveDrees@fullsail.edu>
2014-01-31 12:23:37 -0500

    WPF Practical Exam
    Problem 1 done

---
+++ WPF/Misc/js/WPFpracticalexam.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Steve Drees on 1/31/14, Assignment: WPF practical exam.
+ */
+//Problem 1
+//A.	Create a variable named practicalVariable1. Set this variable equal to 1337.
+//B.	Create an Array named practicalArray1. Set it the first 4 values equal to 1, 3, 5 & 7
+//C.	Create a loop that loops over the practicalArray1 array.
+//D.	Each time the loop runs document.write() the value of the array.
+
+var practicalVariable1 = 1337;
+var practicalArray1 = [1,3,5,7];
+
+for (var i = 0; i<practicalArray1.length; i++)
+{
+    document.write(practicalArray1);
+}
+
+function problem2(){
+
+}
+
+function problem3(){
+
+}
